9c643efa3fc4dfa7cad4e2190fc4296c3b35eaa6
Jexus <zetanollyjs-cp@yahoo.com.tw>
2017-10-29 23:31:26 +0800

    add tgz file compressed

---
+++ b05901033_hw4/src/mem/memCmd.cpp
@@ -0,0 +1,335 @@
+/****************************************************************************
+  FileName     [ memCmd.cpp ]
+  PackageName  [ mem ]
+  Synopsis     [ Define memory test commands ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2007-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include "memCmd.h"
+#include "memTest.h"
+#include "cmdParser.h"
+#include "util.h"
+
+using namespace std;
+
+extern MemTest mtest;  // defined in memTest.cpp
+
+bool
+initMemCmd()
+{
+   if (!(cmdMgr->regCmd("MTReset", 3, new MTResetCmd) &&
+         cmdMgr->regCmd("MTNew", 3, new MTNewCmd) &&
+         cmdMgr->regCmd("MTDelete", 3, new MTDeleteCmd) &&
+         cmdMgr->regCmd("MTPrint", 3, new MTPrintCmd)
+      )) {
+      cerr << "Registering \"mem\" commands fails... exiting" << endl;
+      return false;
+   }
+   return true;
+}
+
+
+//----------------------------------------------------------------------
+//    MTReset [(size_t blockSize)]
+//----------------------------------------------------------------------
+CmdExecStatus
+MTResetCmd::exec(const string& option)
+{
+   // check option
+   string token;
+   if (!CmdExec::lexSingleOption(option, token))
+      return CMD_EXEC_ERROR;
+   if (token.size()) {
+      int b;
+      if (!myStr2Int(token, b) || b < int(toSizeT(sizeof(MemTestObj)))) {
+         cerr << "Illegal block size (" << token << ")!!" << endl;
+         return CmdExec::errorOption(CMD_OPT_ILLEGAL, token);
+      }
+      #ifdef MEM_MGR_H
+      mtest.reset(toSizeT(b));
+      #else
+      mtest.reset();
+      #endif // MEM_MGR_H
+   }
+   else
+      mtest.reset();
+   return CMD_EXEC_DONE;
+}
+
+void
+MTResetCmd::usage(ostream& os) const
+{  
+   os << "Usage: MTReset [(size_t blockSize)]" << endl;
+}
+
+void
+MTResetCmd::help() const
+{  
+   cout << setw(15) << left << "MTReset: " 
+        << "(memory test) reset memory manager" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    MTNew <(size_t numObjects)> [-Array (size_t arraySize)]
+//----------------------------------------------------------------------
+CmdExecStatus
+MTNewCmd::exec(const string& option)
+{
+   // TODO.Done
+      // check option
+      vector<string> options;
+      if (!CmdExec::lexOptions(option, options))
+         return CMD_EXEC_ERROR;
+
+      if (options.empty())
+         return CmdExec::errorOption(CMD_OPT_MISSING, "");
+   
+      //bool doArr = false;
+      int numObjects = 0;
+      int arraySize = 0;
+      if (options.size() == 1)
+      {
+        if(!myStr2Int(options[0], numObjects)) 
+                return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[0]);
+        if(numObjects<=0)
+                return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[0]);
+        // allocate mem
+        try{
+        mtest.newObjs(numObjects);
+        }
+        catch(bad_alloc& cat){
+                #ifdef MEM_DEBUG
+                cerr << "catch you!!!\n";
+                #endif
+        }
+      }
+      else{
+        for (unsigned int i = 0; i < options.size(); i++){
+                if(myStrNCmp("-Array", options[i], 2) == 0){
+                        if ((i+1)>=options.size()){
+                                return CmdExec::errorOption(CMD_OPT_MISSING, options[i]);
+                        }
+                        else{
+                                if(!myStr2Int(options[++i], arraySize)) 
+                                        return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                                
+                                if(arraySize<=0)
+                                        return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                                
+                        }
+                }
+                else if(numObjects==0){
+                        if(!myStr2Int(options[i], numObjects)) 
+                                return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                        if(numObjects<=0)
+                                return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                }
+                else{
+                        return CmdExec::errorOption(CMD_OPT_EXTRA, options[i]);
+                }
+        }
+        if(numObjects==0)
+                return CmdExec::errorOption(CMD_OPT_MISSING, "");
+        //allocate mem
+        try{
+        mtest.newArrs((size_t)numObjects, (size_t)arraySize);
+        }
+        catch(bad_alloc& cat){
+                #ifdef MEM_DEBUG
+                cerr << "catch you!!!\n";
+                #endif
+        }
+      }
+
+   return CMD_EXEC_DONE;
+}
+
+void
+MTNewCmd::usage(ostream& os) const
+{  
+   os << "Usage: MTNew <(size_t numObjects)> [-Array (size_t arraySize)]\n";
+}
+
+void
+MTNewCmd::help() const
+{  
+   cout << setw(15) << left << "MTNew: " 
+        << "(memory test) new objects" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    MTDelete <-Index (size_t objId) | -Random (size_t numRandId)> [-Array]
+//----------------------------------------------------------------------
+CmdExecStatus
+MTDeleteCmd::exec(const string& option)
+{
+   // TODO.Done
+   vector<string> options;
+   if (!CmdExec::lexOptions(option, options))
+      return CMD_EXEC_ERROR;
+
+   if (options.empty())
+      return CmdExec::errorOption(CMD_OPT_MISSING, "");
+
+   bool doArr = false;
+   string raname;
+   int objId = -1;
+   string objIdstr;
+   int numRandId = -1;
+   string numRandIdstr;
+   for (size_t i = 0; i < options.size(); i++)
+   {
+           if (myStrNCmp("-Array", options[i], 2) == 0)
+           {
+                if (doArr!=false) return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                doArr = true;
+           }
+           else if (myStrNCmp("-Index", options[i], 2) == 0)
+           {
+                   if (objId!=-1 || numRandId!=-1)
+                           return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                   if ((i + 1) >= options.size())
+                   {
+                           return CmdExec::errorOption(CMD_OPT_MISSING, options[i]);
+                   }
+                   else
+                   {
+                           if (!myStr2Int(options[++i], objId))
+                                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                           objIdstr = options[i];
+                           if (objId < 0)
+                                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                   }
+           }
+           else if (myStrNCmp("-Random", options[i], 2) == 0)
+           {
+                   raname = options[i];
+                   if (objId != -1 || numRandId != -1)
+                   {
+                           #ifdef MEM_DEBUG
+                           cout << "redifine!" ; 
+                           #endif
+                           return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                   }
+                   if ((i + 1) >= options.size())
+                   {
+                           return CmdExec::errorOption(CMD_OPT_MISSING, options[i]);
+                   }
+                   else
+                   {
+                           i++;
+                           if (!myStr2Int(options[i], numRandId)){
+                                   #ifdef MEM_DEBUG
+                                   cout << "mystr2int fail!"; 
+                                   #endif
+                                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                           }
+                           numRandIdstr = options[i];
+                           if (numRandId <= 0){
+                                   #ifdef MEM_DEBUG
+                                   cout << "numRandId <= 0"; 
+                                   #endif
+                                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+                           }
+                   }
+           }
+           else
+           {
+                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, options[i]);
+           }
+     }
+             if(doArr){
+                     
+                     //_arrList[objId] = 0;
+                     if(objId!=-1){
+                        if((size_t)objId>=mtest.getArrListSize()){
+                                cerr << "Size of array list ("<< mtest.getArrListSize() <<") is <= " << objIdstr <<"!!"<< endl;
+                                return CmdExec::errorOption(CMD_OPT_ILLEGAL, objIdstr);
+                        }
+                        else mtest.deleteArr((size_t)objId);
+                     }
+                     else if (numRandId!=-1){
+                                if (mtest.getArrListSize()==0){
+                                cerr << "Size of array list is 0!!"<< endl;
+                                return CmdExec::errorOption(CMD_OPT_ILLEGAL, raname);
+                        }
+                             for (int i = 0; i < numRandId;i++){
+                                     mtest.deleteArr((size_t)rnGen((int)mtest.getArrListSize()));
+                             }
+                     }
+             }
+             else{
+                        
+                        //_objList[objId] = 0;
+
+                        if(objId!=-1){
+                                if((size_t)objId>=mtest.getObjListSize()){
+                                        //#ifdef MEM_DEBUG
+                                        cerr << "Size of object list ("<< mtest.getObjListSize() <<") is <= " << objIdstr <<"!!"<< endl;
+                                        //#endif // MEM_DEBUG
+                                        return CmdExec::errorOption(CMD_OPT_ILLEGAL, objIdstr);
+                                }
+                                else mtest.deleteObj((size_t)objId);
+                        }
+                        else if (numRandId!=-1){
+                                if (mtest.getObjListSize()==0){
+                                   cerr << "Size of object list is 0!!"<< endl;
+                                   return CmdExec::errorOption(CMD_OPT_ILLEGAL, raname);
+                              }
+                                for (int i = 0; i < numRandId;i++){
+                                        mtest.deleteObj((size_t)rnGen((int)mtest.getObjListSize()));
+                                }
+                        }
+             }
+     
+   
+   return CMD_EXEC_DONE;
+}
+
+void
+MTDeleteCmd::usage(ostream& os) const
+{  
+   os << "Usage: MTDelete <-Index (size_t objId) | "
+      << "-Random (size_t numRandId)> [-Array]" << endl;
+}
+
+void
+MTDeleteCmd::help() const
+{  
+   cout << setw(15) << left << "MTDelete: " 
+        << "(memory test) delete objects" << endl;
+}
+
+
+//----------------------------------------------------------------------
+//    MTPrint
+//----------------------------------------------------------------------
+CmdExecStatus
+MTPrintCmd::exec(const string& option)
+{
+   // check option
+   if (option.size())
+      return CmdExec::errorOption(CMD_OPT_EXTRA, option);
+   mtest.print();
+
+   return CMD_EXEC_DONE;
+}
+
+void
+MTPrintCmd::usage(ostream& os) const
+{  
+   os << "Usage: MTPrint" << endl;
+}
+
+void
+MTPrintCmd::help() const
+{  
+   cout << setw(15) << left << "MTPrint: " 
+        << "(memory test) print memory manager info" << endl;
+}
+
+
---
+++ b05901033_hw4/src/mem/memMgr.h
@@ -0,0 +1,475 @@
+/****************************************************************************
+  FileName     [ memMgr.h ]
+  PackageName  [ cmd ]
+  Synopsis     [ Define Memory Manager ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2007-present LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#ifndef MEM_MGR_H
+#define MEM_MGR_H
+
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+
+using namespace std;
+
+// Turn this on for debugging
+// #define MEM_DEBUG
+
+//--------------------------------------------------------------------------
+// Define MACROs
+//--------------------------------------------------------------------------
+#define MEM_MGR_INIT(T) \
+MemMgr<T>* const T::_memMgr = new MemMgr<T>
+//declare a memmgr template with datatype T
+
+#define USE_MEM_MGR(T)                                                      \
+public:                                                                     \
+   void* operator new(size_t t) { return (void*)(_memMgr->alloc(t)); }      \
+   void* operator new[](size_t t) { return (void*)(_memMgr->allocArr(t)); } \
+   void  operator delete(void* p) { _memMgr->free((T*)p); }                 \
+   void  operator delete[](void* p) { _memMgr->freeArr((T*)p); }            \
+   static void memReset(size_t b = 0) { _memMgr->reset(b); }                \
+   static void memPrint() { _memMgr->print(); }                             \
+private:                                                                    \
+   static MemMgr<T>* const _memMgr
+
+// You should use the following two MACROs whenever possible to 
+// make your code 64/32-bit platform independent.
+// DO NOT use 4 or 8 for sizeof(size_t) in your code
+//
+#define SIZE_T      sizeof(size_t)
+#define SIZE_T_1    (sizeof(size_t) - 1)
+
+// TODO.Done: Define them by SIZE_T and/or SIZE_T_1 MACROs.
+//
+// To promote 't' to the nearest multiple of SIZE_T; 
+// e.g. Let SIZE_T = 8;  toSizeT(7) = 8, toSizeT(12) = 16
+#define toSizeT(t)      ( t%SIZE_T ? t + SIZE_T - (t%SIZE_T) : t)   // TODO.Done
+//
+// To demote 't' to the nearest multiple of SIZE_T
+// e.g. Let SIZE_T = 8;  downtoSizeT(9) = 8, downtoSizeT(100) = 96
+#define downtoSizeT(t)  t - (t%SIZE_T)  // TODO.Done
+
+// R_SIZE is the size of the recycle list
+#define R_SIZE 256
+
+//--------------------------------------------------------------------------
+// Forward declarations
+//--------------------------------------------------------------------------
+template <class T> class MemMgr;
+
+
+//--------------------------------------------------------------------------
+// Class Definitions
+//--------------------------------------------------------------------------
+// T is the class that use this memory manager
+//
+// Make it a private class;
+// Only friend to MemMgr;
+//
+template <class T>
+class MemBlock
+{
+   friend class MemMgr<T>;
+
+   // Constructor/Destructor
+   MemBlock(MemBlock<T>* n, size_t b) : _nextBlock(n) {
+      _begin = _ptr = new char[b]; _end = _begin + b; }
+   ~MemBlock() { delete [] _begin; }
+
+   // Member functions
+   void reset() { _ptr = _begin; }
+   // 1. Get (at least) 't' bytes memory from current block
+   //    Promote 't' to a multiple of SIZE_T
+   // 2. Update "_ptr" accordingly
+   // 3. The return memory address is stored in "ret"
+   // 4. Return false if not enough memory
+   bool getMem(size_t t, T*& ret) {
+      // TODO.Done
+      t = toSizeT(t);
+      if(_ptr + t > _end) return false;
+      ret = (T *)_ptr;
+      _ptr = _ptr + t;
+      return true;
+   }
+   size_t getRemainSize() const { return size_t(_end - _ptr); }
+      
+   MemBlock<T>* getNextBlock() const { return _nextBlock; }
+
+   // Data members
+   char*             _begin;
+   char*             _ptr;
+   char*             _end;
+   MemBlock<T>*      _nextBlock;
+};
+
+// Make it a private class;
+// Only friend to MemMgr;
+//
+template <class T>
+class MemRecycleList
+{
+   friend class MemMgr<T>;
+
+   // Constructor/Destructor
+   MemRecycleList(size_t a = 0) : _arrSize(a), _first(0), _nextList(0) {}
+   ~MemRecycleList() { reset(); }
+
+   // Member functions
+   // ----------------
+   size_t getArrSize() const { return _arrSize; }
+   MemRecycleList<T>* getNextList() const { return _nextList; }
+   void setNextList(MemRecycleList<T>* l) { _nextList = l; }
+   // pop out the first element in the recycle list
+   T* popFront() {
+      // TODO.Done
+      T *target = _first;
+      _first = getNext(_first);
+      return target;
+   }
+   // push the element 'p' to the beginning of the recycle list
+   void  pushFront(T* p) {
+      // TODO.Done
+      //T *tmp = p;
+      *((size_t *)p) = (size_t)_first;
+      _first = p;
+   }
+   // Release the memory occupied by the recycle list(s)
+   // DO NOT release the memory occupied by MemMgr/MemBlock
+   void reset() {
+      // TODO.Done
+      //while(_first!=0){
+      //  T *target = _first;
+      //  _first = getNext(_first);
+      //  delete target;
+      //}
+      if(_nextList){
+          delete _nextList;
+          _nextList->reset();
+	}
+      _nextList = 0;
+      _first = 0;
+   }
+
+   // Helper functions
+   // ----------------
+   // Iterate to the next element after 'p' in the recycle list
+   T* getNext(T* p) const {
+      // TODO.Done
+      if (p==0) return 0;
+      return (T*)(*(size_t*)p);
+   }
+   //
+   // count the number of elements in the recycle list
+   size_t numElm() const {
+      size_t count = 0;
+      T* p = _first;
+      while (p) {
+         p = getNext(p);
+         ++count;
+      }
+      return count;
+   }
+
+   // Data members
+   size_t              _arrSize;   // the array size of the recycled data
+   T*                  _first;     // the first recycled data
+   MemRecycleList<T>*  _nextList;  // next MemRecycleList
+                                   //      with _arrSize + x*R_SIZE
+};
+
+template <class T>
+class MemMgr
+{
+   #define S sizeof(T)
+
+public:
+   MemMgr(size_t b = 65536) : _blockSize(b) {
+      assert(b % SIZE_T == 0);
+      _activeBlock = new MemBlock<T>(0, _blockSize);
+      for (int i = 0; i < R_SIZE; ++i)
+         _recycleList[i]._arrSize = i;
+   }
+   ~MemMgr() { reset(); delete _activeBlock; }
+
+   // 1. Remove the memory of all but the firstly allocated MemBlocks
+   //    That is, the last MemBlock searchd from _activeBlock.
+   //    reset its _ptr = _begin (by calling MemBlock::reset())
+   // 2. reset _recycleList[]
+   // 3. 'b' is the new _blockSize; "b = 0" means _blockSize does not change
+   //    if (b != _blockSize) reallocate the memory for the first MemBlock
+   // 4. Update the _activeBlock pointer
+   void reset(size_t b = 0) {
+      assert(b % SIZE_T == 0);
+      #ifdef MEM_DEBUG
+      cout << "Resetting memMgr...(" << b << ")" << endl;
+      #endif // MEM_DEBUG
+      // TODO.Done
+      if(b==0||b==_blockSize){
+        //detele all Blocks except 1st Block
+        while(_activeBlock->_nextBlock!=0){
+          MemBlock<T> *target = _activeBlock;
+          _activeBlock = _activeBlock->_nextBlock;
+          delete target;
+        }
+  _activeBlock->reset();
+  for (size_t i = 0; i < R_SIZE; i++)
+  _recycleList[i].reset();
+      }
+      else if(b!=_blockSize){
+        //delete all Blocks
+        _blockSize = b;
+        while (_activeBlock->_nextBlock != 0)
+        {
+          MemBlock<T> *target = _activeBlock;
+          _activeBlock = _activeBlock->_nextBlock;
+          delete target;
+        }
+	_activeBlock->reset();
+        delete _activeBlock;
+        _activeBlock = new MemBlock<T>(0, _blockSize);
+
+	for (size_t i = 0; i < R_SIZE; i++)
+         _recycleList[i].reset();
+      }
+   }
+   // Called by new
+   T* alloc(size_t t) {
+      assert(t == S);
+      #ifdef MEM_DEBUG
+      cout << "Calling alloc...(" << t << ")" << endl;
+      #endif // MEM_DEBUG
+      return getMem(t);
+   }
+   // Called by new[]
+   T* allocArr(size_t t) {
+      #ifdef MEM_DEBUG
+      cout << "Calling allocArr...(" << t << ")" << endl;
+      #endif // MEM_DEBUG
+      // Note: no need to record the size of the array == > system will do
+      return getMem(t);
+   }
+   // Called by delete
+   void  free(T* p) {
+      #ifdef MEM_DEBUG
+      cout << "Calling free...(" << p << ")" << endl;
+      #endif // MEM_DEBUG
+      getMemRecycleList(0)->pushFront(p);
+   }
+   // Called by delete[]
+   void  freeArr(T* p) {
+      #ifdef MEM_DEBUG
+      cout << "Calling freeArr...(" << p << ")" << endl;
+      #endif // MEM_DEBUG
+      // TODO.Done
+      // Get the array size 'n' stored by system,
+      // which is also the _recycleList index
+      size_t n = *((size_t*)p);
+      //n = getArraySize(sizeof(*p)/sizeof(T));
+      #ifdef MEM_DEBUG
+      cout << ">> Array size = " << n << endl;
+      cout << "Recycling " << p << " to _recycleList[" << n << "]" << endl;
+      #endif // MEM_DEBUG
+      // add to recycle list...
+      getMemRecycleList(n)->pushFront(p);
+   }
+   void print() const {
+      cout << "=========================================" << endl
+           << "=              Memory Manager           =" << endl
+           << "=========================================" << endl
+           << "* Block size            : " << _blockSize << " Bytes" << endl
+           << "* Number of blocks      : " << getNumBlocks() << endl
+           << "* Free mem in last block: " << _activeBlock->getRemainSize()
+           << endl
+           << "* Recycle list          : " << endl;
+      int i = 0, count = 0;
+      while (i < R_SIZE) {
+         const MemRecycleList<T>* ll = &(_recycleList[i]);
+         while (ll != 0) {
+            size_t s = ll->numElm();
+            if (s) {
+               cout << "[" << setw(3) << right << ll->_arrSize << "] = "
+                    << setw(10) << left << s;
+               if (++count % 4 == 0) cout << endl;
+            }
+            ll = ll->_nextList;
+         }
+         ++i;
+      }
+      cout << endl;
+   }
+
+private:
+   size_t                     _blockSize;
+   MemBlock<T>*               _activeBlock;
+   MemRecycleList<T>          _recycleList[R_SIZE];
+
+   // Private member functions
+   //
+   // t: #Bytes; MUST be a multiple of SIZE_T
+   // return the size of the array with respect to memory size t
+   // [Note] t must >= S    p.s. S is the size of <T> class object
+   // Example: SIZE_T = 8
+   //          S = 36
+   //          t % 8 =0 && t >= S
+   // [NOTE] Use this function in (at least) getMem() to get the size of array
+   //        and call getMemRecycleList() later to get the index for
+   //        the _recycleList[]
+   size_t getArraySize(size_t t) const {
+      assert(t % SIZE_T == 0);
+      assert(t >= S);
+      // TODO.Done
+      size_t _S = toSizeT(S);
+      if (t == _S)
+      {
+        return 0;
+      }
+      else{
+        return (t - SIZE_T) / S;
+      }
+      return 0;
+   }
+   // Go through _recycleList[m], its _nextList, and _nexList->_nextList, etc,
+   //    to find a recycle list whose "_arrSize" == "n"
+   // If not found, create a new MemRecycleList with _arrSize = n
+   //    and add to the last MemRecycleList
+   // So, should never return NULL
+   // [Note]: This function will be called by MemMgr->getMem() to get the
+   //         recycle list. Therefore, the recycle list is first created
+   //         by the MTNew command, not MTDelete.
+   MemRecycleList<T>* getMemRecycleList(size_t n) {
+      size_t m = n % R_SIZE;
+      // TODO.Done
+      if(n<R_SIZE){
+        MemRecycleList<T> *current = &_recycleList[n];
+	//assert(current->getArrSize() == n);
+        return current;
+      }
+      if(_recycleList[m]._nextList==0){
+        _recycleList[m]._nextList = new MemRecycleList<T>(n);
+        //_recycleList[m]._nextList->_arrSize = n;
+        return _recycleList[m]._nextList;
+      }
+      MemRecycleList<T> *doing = _recycleList[m]._nextList;
+
+      //MemRecycleList<T> &body = *(_recycleList[m]._nextList);
+      while (doing != 0)
+      {
+        if(doing->_arrSize == n){
+          return doing;
+        }
+        else if(doing->_nextList == 0){
+          doing->_nextList = new MemRecycleList<T>(n);
+          //doing->_nextList->_arrSize = n;
+          return doing->_nextList;
+        }
+        else{
+          //body = *((*doing)._nextList);
+          doing = doing->_nextList;
+        }
+      }
+
+      return doing->_nextList;
+    }
+   // t is the #Bytes requested from new or new[]
+   // Note: Make sure the returned memory is a multiple of SIZE_T
+   T* getMem(size_t t) {
+      T* ret = 0;
+      #ifdef MEM_DEBUG
+      cout << "Calling MemMgr::getMem...(" << t << ")" << endl;
+      #endif // MEM_DEBUG
+      // TODO.Done ---
+      // 1. Make sure to promote t to a multiple of SIZE_T
+      size_t _t = toSizeT(t);
+      size_t n = getArraySize(_t);
+      // 2. Check if the requested memory is greater than the block size.
+      //    If so, throw a "bad_alloc()" exception.
+      //    cerr << "Requested memory (" << t << ") is greater than block size"
+      //         << "(" << _blockSize << "). " << "Exception raised...\n";
+      if (_t>_blockSize){
+        cerr << "Requested memory (" << _t << ") is greater than block size"
+             << "(" << _blockSize << "). " << "Exception raised...\n";
+        throw bad_alloc();
+      }
+      // 3. Check the _recycleList first...
+      //    #ifdef MEM_DEBUG
+      //    cout << "Recycled from _recycleList[" << n << "]..." << ret << endl;
+      //    #endif // MEM_DEBUG
+      //    => 'n' is the size of array
+      //    => "ret" is the return address
+      ret = getMemRecycleList(n)->popFront();
+      if (ret != 0)
+      {
+      #ifdef MEM_DEBUG
+          cout << "Recycled from _recycleList[" << n << "]..." << ret << endl;
+      #endif // MEM_DEBUG
+      //return ret;
+      }
+
+      // If no match from recycle list...
+      // 4. Get the memory from _activeBlock
+      // 5. If not enough, recycle the remained memory and print out ---
+      //    Note: recycle to the as biggest array index as possible
+      //    Note: rn is the array size
+      //    #ifdef MEM_DEBUG
+      //    cout << "Recycling " << ret << " to _recycleList[" << rn << "]\n";
+      //    #endif // MEM_DEBUG
+      //    ==> allocate a new memory block, and print out ---
+      //    #ifdef MEM_DEBUG
+      //    cout << "New MemBlock... " << _activeBlock << endl;
+      //    #endif // MEM_DEBUG
+      else if(!_activeBlock->MemBlock<T>::getMem(_t,ret)){//no enough space
+        size_t _S = toSizeT(S);
+        if (_activeBlock->getRemainSize() >= _S)
+        {
+          size_t rn = getArraySize(downtoSizeT(_activeBlock->getRemainSize()));
+          size_t space;
+          if (rn == 0) space = S;
+          else space = SIZE_T+rn*S;
+          _activeBlock->MemBlock<T>::getMem(space, ret);
+          getMemRecycleList(rn)->pushFront(ret);
+          #ifdef MEM_DEBUG
+            cout << "Recycling " << ret << " to _recycleList[" << rn << "]\n";
+          #endif // MEM_DEBUG
+        }
+          _activeBlock = new MemBlock<T>(_activeBlock,_blockSize);
+          #ifdef MEM_DEBUG
+            cout << "New MemBlock... " << _activeBlock << endl;
+          #endif // MEM_DEBUG
+          _activeBlock->getMem(_t, ret);
+          //ret =  (T*)(_activeBlock->_ptr);
+          //return _activeBlock._ptr;
+      }
+      else{
+        //enough! do nothing!
+        //return ret;
+      }
+      // 6. At the end, print out the acquired memory address
+      //    #ifdef MEM_DEBUG
+      //    cout << "Memory acquired... " << ret << endl;
+      //    #endif // MEM_DEBUG
+      #ifdef MEM_DEBUG
+          cout << "Memory acquired... " << ret << endl;
+      #endif // MEM_DEBUG
+      return ret;
+   }
+   // Get the currently allocated number of MemBlock's
+   size_t getNumBlocks() const {
+      // TODO.Done
+      MemBlock<T> *countptr = _activeBlock;
+      size_t count = 1;
+      while (countptr->_nextBlock != 0)
+      {
+        count++;
+        countptr = countptr->_nextBlock;
+      }
+      return count;
+   }
+
+};
+
+#endif // MEM_MGR_H
